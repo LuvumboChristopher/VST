ea16357485bdcbc1c1f00f3d72862afb9fdc87aa
Christopher Luvumbo <christopherluvumbo@gmail.com>
2023-04-03 12:26:19 +0200

    Update Navbar.js

--- src/pages/Home/components/Navbar/Navbar.js
+++ src/pages/Home/components/Navbar/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
 
         <NavLink>
           <NavIcon
-            href='/vinylstorelyonshop.vercel.app'
+            href='https://vinylstorelyonshop.vercel.app/'
           ></NavIcon>
           <NavTitle>Store</NavTitle>
         </NavLink>
