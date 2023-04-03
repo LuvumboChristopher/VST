90dcc12f484882af37077db205d4773524f98138
Christopher Luvumbo <christopherluvumbo@gmail.com>
2023-04-03 09:00:20 +0200

    Update style.js

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -87,7 +87,7 @@ export const NavTitle = styled.p`
   display: none;
   font-size: 0.93rem;
   color: rgb(255, 255, 255);
-  -webkit-text-stroke: 1px black;
+  text-shadow: 0.05em 0.05em 0 #000, 0.05em 0.05em 0 #000, 0.05em 0.05em 0 #000, 0.05em 0.05em 0 #000;
   ${NavLink}:hover & {
     display: block;
   }
